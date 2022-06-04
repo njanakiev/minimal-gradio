79acf67bc936826a14bf9bdde1fd5bf11db801ae
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2022-06-04 13:31:06 +0200

    Minor fix

--- minimal/app.py
+++ minimal/app.py
@@ -8,7 +8,9 @@ if __name__ == '__main__':
     iface = gr.Interface(
         fn=greet,
         inputs="text",
-        outputs="text")
+        outputs="text",
+        description="Minimal Gradio Example Application",
+        flagging_dir='flagged')
     iface.launch(
         server_name='0.0.0.0',
         server_port=7861)
